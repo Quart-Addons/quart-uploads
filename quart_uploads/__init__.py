c928fd77268f6ecdaa993a4c6a1a80d69db95320
Chris Rood <crood58@gmail.com>
2022-07-31 14:27:16 +0000

    Added utils.

--- quart_uploads/__init__.py
+++ quart_uploads/__init__.py
@@ -6,11 +6,29 @@ from .exceptions import UploadNotAllowed, AllExcept
 from .file_ext import (TEXT, DOCUMENTS, IMAGES, AUDIO, DATA, SCRIPTS,
                        ARCHIVES, SOURCE, EXECUTABLES, DEFAULTS, ALL)
 from .set import UploadSet
-from .utils import patch_request_class, TestingFileStorage
+from .utils import (extension, lowercase_ext, addslash,
+                   patch_request_class, TestingFileStorage)
 
-__all__ = ['config_for_set', 'configure_uploads', 'TEXT', 'DOCUMENTS',
-          'IMAGES', 'AUDIO', 'DATA', 'SCRIPTS', 'ARCHIVES', 'SOURCE',
-          'EXECUTABLES', 'DEFAULTS', 'ALL', 'UploadSet', 'patch_request_class',
-          'TestingFileStorage']
+__all__ = [
+    'config_for_set',
+    'configure_uploads',
+    'TEXT',
+    'DOCUMENTS',
+    'IMAGES',
+    'AUDIO',
+    'DATA',
+    'SCRIPTS',
+    'ARCHIVES',
+    'SOURCE',
+    'EXECUTABLES',
+    'DEFAULTS',
+    'ALL',
+    'UploadSet',
+    'extension',
+    'lowercase_ext',
+    'addslash',
+    'patch_request_class',
+    'TestingFileStorage'
+    ]
 
 __version__ = '0.0.1'
