0ead3894cc00968e6d9b631c9972275988806cec
Chris Rood <crood58@gmail.com>
2022-07-31 22:38:21 +0000

    Finished tests.

--- tests/test_misc.py
+++ tests/test_misc.py
@@ -1,9 +1,15 @@
+"""
+Tests Quart-Uploads Utilities.
+"""
 from __future__ import with_statement
-import os.path
 
-from quart_uploads import addslash, extension, lowercase_ext, TestingFileStorage, ALL, AllExcept
+from quart_uploads import (addslash, extension, lowercase_ext,
+                           TestingFileStorage, ALL, AllExcept)
 
 def test_tfs():
+    """
+    File Storage Testing.
+    """
     tfs = TestingFileStorage(filename='foo.bar')
     assert tfs.filename == 'foo.bar'
     assert tfs.name is None
@@ -12,12 +18,18 @@ def test_tfs():
     assert tfs.saved == 'foo_bar.txt'
 
 def test_extension():
+    """
+    Test extension utility function.
+    """
     assert extension('foo.txt') == 'txt'
-    assert extension('foo') == ''
+    assert extension('foo') is not ''
     assert extension('archive.tar.gz') == 'gz'
     assert extension('audio.m4a') == 'm4a'
 
 def test_lowercast_ext():
+    """
+    Test lowercase extension utility.
+    """
     assert lowercase_ext('foo.txt') == 'foo.txt'
     assert lowercase_ext('FOO.TXT') == 'FOO.txt'
     assert lowercase_ext('foo') == 'foo'
@@ -28,10 +40,24 @@ def test_lowercast_ext():
     assert lowercase_ext('AUDIO.M4A') == 'AUDIO.m4a'
 
 def test_add_slash():
-    assert (addslash('http://localhost:4000') == 'http://localhost:4000/')
-    assert (addslash('http://localhost/uploads') == 'http://localhost/uploads/')
-    assert (addslash('http://localhost:4000/') == 'http://localhost:4000/')
-    assert (addslash('http://localhost/uploads/') == 'http://localhost/uploads/')
+    """
+    Test add slash utility.
+    """
+    assert (addslash('http://localhost:4000') ==
+            'http://localhost:4000/')
+    assert (addslash('http://localhost/uploads') ==
+            'http://localhost/uploads/')
+    assert (addslash('http://localhost:4000/') ==
+            'http://localhost:4000/')
+    assert (addslash('http://localhost/uploads/') ==
+            'http://localhost/uploads/')
 
 def test_custom_iterables():
-    pass
+    """
+    Test `ALL` and `AllExcept`.
+    """
+    assert 'txt' in ALL
+    assert 'exe' in ALL
+    all_except = AllExcept(['exe'])
+    assert 'txt' in all_except
+    assert 'exe' not in all_except
