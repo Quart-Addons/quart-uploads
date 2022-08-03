412058a3285f025cafa89bdd9af68fd3320a51ea
Chris Rood <crood58@gmail.com>
2022-08-03 01:11:35 +0000

    Added package info.

--- quart_uploads/__init__.py
+++ quart_uploads/__init__.py
@@ -1,6 +1,9 @@
 """
 quart_uploads
 """
+from __future__ import absolute_import
+from __future__ import print_function
+
 from .config import UploadConfiguration, config_for_set, configure_uploads
 from .exceptions import UploadNotAllowed, AllExcept
 from .file_ext import (TEXT, DOCUMENTS, IMAGES, AUDIO, DATA, SCRIPTS,
@@ -31,4 +34,8 @@ __all__ = [
     'TestingFileStorage'
     ]
 
-__version__ = '0.0.1'
+__version_info__ = (0, 0, 1)
+__version__ = '.'.join(__version_info__)
+__author__ = 'Chris Rood'
+__license__ = 'MIT'
+__copyright__ = '(c) 2022 by Chris Rood'
