ec52c68c9a54059cec62d00b47998e4f7d6de5e1
Chris Rood <crood58@gmail.com>
2022-08-03 03:25:15 +0000

    Updated file.

--- example/photolog.py
+++ example/photolog.py
@@ -1,5 +1,11 @@
 """
 photolog.py
 ===========
-This is a simple example app for Quart-Uploads. 
+This is a simple example app for Quart-Uploads. It is a
+simple photolog app that lets you submit blog posts that
+are photos.
 """
+from quart import (Quart, request, url_for, redirect, render_template,
+                  flash, session, g)
+
+from quart_uploads import UploadSet, configure_uploads, IMAGES, UploadNotAllowed
