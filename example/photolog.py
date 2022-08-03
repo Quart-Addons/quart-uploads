ca00f0080fd25f83e1ab584a8f5106bd0ca998d4
Chris Rood <crood58@gmail.com>
2022-08-03 03:28:36 +0000

    Updated file.

--- example/photolog.py
+++ example/photolog.py
@@ -9,3 +9,10 @@ from quart import (Quart, request, url_for, redirect, render_template,
                   flash, session, g)
 
 from quart_uploads import UploadSet, configure_uploads, IMAGES, UploadNotAllowed
+
+DEBUG = False
+SECRET_KEY = ('\xa3\xb6\x15\xe3E\xc4\x8c\xbaT\x14\xd1:'
+              '\xafc\x9c|.\xc0H\x8d\xf2\xe5\xbd\xd5')
+
+UPLOADED_PHOTOS_DEST = '/tmp/photolog'
+
