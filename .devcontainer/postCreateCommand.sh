437029221fb4fa3fddbc5bba9cf3471d4ba83738
Chris Rood <crood58@gmail.com>
2022-08-05 02:34:26 +0000

    Updated dev container to use poetry.

---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# Determines if poetry toml file exists and if it does will install the dependecies. 
+
+# Stop on errors
+set -ex
+
+POETRY_FILE=pyproject.toml
+
+if [ -f "$POETRY_FILE" ]; then
+    poetry install --no-interaction --no-ansi --extras "docs"
+else
+    echo "There is no pyproject file. You will need to create one and rebuild the container."
+fi
+
