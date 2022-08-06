d79d693ef5dba0ceff45bfc7340a8d2bb887e237
Chris Rood <crood58@gmail.com>
2022-08-06 17:20:23 +0000

    Updated example.

---
+++ example/README.rst
@@ -0,0 +1,11 @@
+Tutorial: Building a simple photolog
+================================
+
+This is the example code for the photolog tutorial. It is managed using
+`Poetry <https://python-poetry.org>`_ with the following commands,
+
+.. code-block:: console
+
+    poetry install
+    poetry run start
+    poetry run pytest -c pyproject.toml tests/
--- example/photolog/__init__.py
+++ example/photolog/__init__.py
@@ -5,9 +5,10 @@ This is a simple example app for Quart-Uploads. It is a
 simple photolog app that lets you submit blog posts that
 are photos.
 """
+from quart import flask_patch
 from quart import (Quart, request, url_for, redirect, render_template,
                   flash, session, g)
-
+from flask_sqlalchemy import SQLAlchemy
 from quart_uploads import UploadSet, configure_uploads, IMAGES, UploadNotAllowed
 
 # default configuration values
@@ -16,6 +17,11 @@ SECRET_KEY = ('\xa3\xb6\x15\xe3E\xc4\x8c\xbaT\x14\xd1:'
               '\xafc\x9c|.\xc0H\x8d\xf2\xe5\xbd\xd5')
 
 UPLOADED_PHOTOS_DEST = '/tmp/photolog'
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/phtolog.db'
+
+ADMIN_USERNAME = 'admin'
+ADMIN_PASSWORD = 'quartftw'
 
 # application
 app = Quart(__name__)
@@ -26,22 +32,48 @@ uploaded_photos = UploadSet('photos', IMAGES)
 configure_uploads(app, uploaded_photos)
 
 # database
+db = SQLAlchemy(app)
 
-# utils 
+class Post(db.Model):
+    """
+    Post Database Model.
+    """
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    filename = db.Column(db.String, nullable=False)
+    caption = db.Column(db.String, nullable=False)
+    published = db.Column(db.DateTime, nullable=False, server_defaults=db.func.current_timestamp())
 
+def init_db() -> None:
+    """
+    Inits the database
+    """
+    db.drop_all()
+    db.create_all()
+
+# utils
 def to_index():
+    """
+    Redirects to the index of the frontend.
+    """
     return redirect(url_for('index'))
 
-# views
+@app.before_first_request
+def login_handle():
+    """Handles user login"""
+    g.logged_in = bool(session.get('logged_in'))
 
+# views
 @app.route('/')
 def index():
-    posts = Post.all()
+    """Index route."""
+    posts = Post.query.order_by(Post.created.desc()).all()
     return render_template('index.html', posts=posts)
 
 
 @app.route('/new', methods=['GET', 'POST'])
 async def new():
+    """New post route."""
     if request.method == 'POST':
         files = await request.files
         form = await request.form
@@ -56,9 +88,8 @@ async def new():
             except UploadNotAllowed:
                 await flash("The upload was not allowed")
             else:
-                post = Post(title=title, caption=caption, filename=filename)
-                post.id = unique_id()
-                post.store()
+                db.session.add(Post(title=title, caption=caption, filename=filename))
+                db.session.commit()
                 await flash("Post successful")
                 return to_index()
     return await render_template('new.html')
@@ -66,12 +97,14 @@ async def new():
 
 @app.route('/login', methods=['GET', 'POST'])
 async def login():
+    """User login route."""
     if session.get('logged_in'):
-        flash("You are already logged in")
+        await flash("You are already logged in")
         return to_index()
     if request.method == 'POST':
-        username = request.form.get('username')
-        password = request.form.get('password')
+        form = await request.form
+        username = form.get('username')
+        password = form.get('password')
         if (username == app.config['ADMIN_USERNAME'] and
             password == app.config['ADMIN_PASSWORD']):
             session['logged_in'] = True
@@ -84,6 +117,7 @@ async def login():
 
 @app.route('/logout')
 async def logout():
+    """User logout route."""
     if session.get('logged_in'):
         session['logged_in'] = False
         await flash("Successfully logged out")
@@ -93,4 +127,5 @@ async def logout():
 
 
 def run() -> Quart:
+    """Runs the Photolog Example"""
     return app
