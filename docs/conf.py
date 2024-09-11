cfbaf841f599958247d50d1c13c5af2ee527a6b5
Chris Rood <crood58@gmail.com>
2024-09-11 20:56:09 +0000

    Updated version no.

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@
 project = 'Quart-Uploads'
 copyright = '2022, Chris Rood'
 author = 'Chris Rood'
-release = '0.0.1'
+release = '0.0.3'
 version = release
 
 # -- General configuration ---------------------------------------------------
