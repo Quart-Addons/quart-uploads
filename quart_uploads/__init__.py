1eb92b569334afc015afb8e3117ef77924c082ca
Chris Rood <crood58@gmail.com>
2024-10-05 20:11:23 +0000

    Removed info at bottom

--- quart_uploads/__init__.py
+++ quart_uploads/__init__.py
@@ -21,11 +21,3 @@ __all__ = [
     'UploadSet',
     'TestingFileStorage'
     ]
-
-"""
-__version_info__ = (0, 0, 1)
-__version__ = '.'.join(__version_info__)
-__author__ = 'Chris Rood'
-__license__ = 'MIT'
-__copyright__ = '(c) 2022 by Chris Rood'
-"""
